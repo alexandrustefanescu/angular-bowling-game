0ec8c3cf99d28633c3a6f7243e75ce0a1859dfac
Alexandru Stefanescu <stefanescualexandru1994@gmail.com>
2026-01-28 18:05:56 +0100

    refactor: removed unnecessary code.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { bowlingRoutes } from './features/bowling/bowling-routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    ...bowlingRoutes,
+];
